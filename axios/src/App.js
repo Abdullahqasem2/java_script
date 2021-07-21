822f2f3977747d7d5e9c55d4b5107710f76fafb6
root <abdullahzq97@gmail.com>
2021-07-21 03:53:38 +0300

    03:53

---
+++ axios/src/App.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import Api from './componants/Api';
+function App() {
+  return (
+    <div>
+      <Api />
+    </div>
+  );
+}
+
+export default App;
---
+++ axios/src/componants/Api.jsx
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+const Api = props => {
+    const [pokemon, setpokemon] = useState([]);
+    const [show, setshow] = useState(false);
+    const List = [];
+
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon')
+            .then(response =>{ setpokemon(response.data)})
+    }, []);
+
+    return (
+        <center>
+            <button onClick={() => setshow(true)} >useEffect</button>
+            {show === true && pokemon.map((pokemon)=> (<div>{pokemon.name}</div>))}
+
+        </center>
+    );
+}
+export default Api;
